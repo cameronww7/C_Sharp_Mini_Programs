7e08a9fb5d54812e208bea9ea4268cfb9dec97b0
cam.w.w <cameronww7@users.noreply.github.com>
2017-09-28 13:02:41 -0700

    Adding Start of Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Mini_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
