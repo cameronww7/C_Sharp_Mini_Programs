cd11abd3a9a1741864dc1f2c93daacaef0e86c66
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-03 14:21:04 -0700

    Starting to learn CSharp/ getting it to work on Visual Studo Code, on Linux... Rough

---
+++ Exploring_C_Sharp/CharVar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CharVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CharVar");
+            //Place code here
+            char c1 = 'a';
+            Console.WriteLine("The value of c1 is " + c1);
+
+            char c2 = '\u0062';
+            Console.WriteLine("The value of c2 is " + c2);
+
+            char symbol = '$';
+            Console.WriteLine("Is number or letter? " + Char.IsLetterOrDigit(symbol));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
+
 namespace C__Mini_Programs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello1 World!");
         }
     }
 }
