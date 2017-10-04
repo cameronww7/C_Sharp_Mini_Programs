2922852b986da30f4528ed4622fc0fa0ae06d7bb
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 16:05:57 -0700

    Messing around wiht Parsing and converting strings to number

---
+++ Exploring_C_Sharp/StringToNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String to Number!");
+
+            //Place code here
+            string s = "255";
+            int intFromString = Int32.Parse(s);
+            Console.WriteLine("Value of intFromString: " + intFromString);
+
+            int doubled = intFromString * 2;
+            Console.WriteLine("Value of doubled: " + doubled);
+
+            string s2 = "19.99";
+            //double doubleFromString = Double.Parse(s2);
+            //Output("Value of doubleFromString: " + doubleFromString);
+
+            double doubleFromString;
+            if (Double.TryParse(s2, out doubleFromString))
+            {
+                Console.WriteLine("Value of doubleFromString: " + doubleFromString);
+            }
+            else
+            {
+                Console.WriteLine("Couldn't parse the value!");
+            }
+        }
+    }
+}
