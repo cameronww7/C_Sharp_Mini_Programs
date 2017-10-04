2a74cf50effb1b7cd70ccfa6744519ef1dbc45a8
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 14:46:01 -0700

    Played with StringBuilder

---
+++ Exploring_C_Sharp/StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String Builder!");
+
+             //Place code here
+            StringBuilder builder = new StringBuilder();
+            builder.Append("Hello");
+
+            Console.WriteLine(builder.ToString());
+
+            builder.Append(", ")
+                .Append("World")
+                .Append("!");
+
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
