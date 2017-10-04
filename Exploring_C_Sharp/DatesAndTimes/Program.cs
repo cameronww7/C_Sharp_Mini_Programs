10c9835732fbc1f0c30c9a93e6e204451b49f558
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 16:22:29 -0700

    checking out Date and time data, Pretty Cool stuff

---
+++ Exploring_C_Sharp/DatesAndTimes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dates and Times!");
+
+            DateTime dt = new DateTime(2014, 1, 1);
+            Console.WriteLine("The date is " + dt.ToString("MMMM d, yyyy"));
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Now is " + now.ToString("MMMM d, yyyy"));
+
+            DateTime anotherDay = dt.AddDays(-1);
+            Console.WriteLine("The date is " + anotherDay.ToString("MMMM d, yyyy"));
+        }
+    }
+}
