4033e3c21dbcc9de0bb00f3a447a0928b75a2f09
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 16:20:16 -0700

    Quick messing with Const and Enums

---
+++ Exploring_C_Sharp/ConstAndEnums/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConstAndEnums
+{
+    class Program
+    {
+        public const int MeaningOfLife = 42;
+
+        enum Flavors { DarkChocolate = 1, Vanilla, Strawberry };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Const and Enums!");
+
+            Console.WriteLine("This is a Const " + MeaningOfLife);
+
+            Console.WriteLine("This is a Enum " + Flavors.Strawberry + " " + Flavors.Vanilla);
+
+            
+        }
+    }
+}
