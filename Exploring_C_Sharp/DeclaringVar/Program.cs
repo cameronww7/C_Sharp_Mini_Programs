a4aa6e71583f418e757a05811635e1094aaa48a4
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 14:26:36 -0700

    Starting DeclareVar Prgram

---
+++ Exploring_C_Sharp/DeclaringVar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DeclaringVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Declaring Var!");
+
+                        //Place code here
+            int i = 42;
+            Console.WriteLine("The value is " + i);
+
+            int meaningOfLife = new Int32();
+            Console.WriteLine("The value is " + meaningOfLife);
+
+            var implicitValue = 56L;
+            Console.WriteLine("The value is " + implicitValue);
+            Console.WriteLine("The variable's type is: " + implicitValue.GetType().ToString());
+
+            byte byteValue = 255;
+            Console.WriteLine("The byte value is " + byteValue);
+
+            byteValue++;
+            Console.WriteLine("The new byte value is " + byteValue);
+        }
+    }
+}
