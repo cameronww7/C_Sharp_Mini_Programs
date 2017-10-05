b2fa0f681c89ac0c6bfb9e89cc993b1ecb9df196
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-04 17:06:05 -0700

    Finished Simple Console Input

---
+++ Exploring_C_Sharp/SimpleConsoleInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Console Input!");
+
+            string input = Console.ReadLine();
+            Console.WriteLine("You entered: " + input);
+        }
+    }
+}
