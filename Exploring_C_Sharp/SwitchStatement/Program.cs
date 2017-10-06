7e3fded02300b247ecd9f91c7f081013615ecb24
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-06 11:01:24 -0700

    Exploring Switch Statement

---
+++ Exploring_C_Sharp/SwitchStatement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch Statement");
+
+            Console.Write("Enter an operation: + - * / ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+" :
+                    Console.WriteLine("You chose to add");
+                    break;
+
+                case "-":
+                    Console.WriteLine("You chose to subtract");
+                    break;
+
+                case "*":
+                    Console.WriteLine("You chose to multiply");
+                    break;
+
+                case "/":
+                    Console.WriteLine("You chose to divide");
+                    break;
+
+                default:
+                    Console.WriteLine("No such operation");
+                    break;
+            }
+        }
+    }
+}
