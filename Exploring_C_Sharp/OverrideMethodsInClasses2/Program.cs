2ecfd56d6ab1f539e5ff296c0cef200d6627941f
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-06 16:22:06 -0700

    Finished simple examples of Inheritance, Overriding inheritance methods, and Polymorphism

---
+++ Exploring_C_Sharp/OverrideMethodsInClasses2/Program.cs
@@ -0,0 +1,37 @@
+﻿using OverrideMethodsInClasses2.Model;
+using System;
+using System.Collections.Generic;
+
+namespace OverrideMethodsInClasses2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Override methods in Classes 2!");
+
+            var produce = new List<object>();
+
+            var f1 = new Fruit("Apple", 8.5, 3);
+            produce.Add(f1);
+
+            var f2 = new Fruit("Banana", 4.5, 7);
+            produce.Add(f2);
+
+            produce.Add(new Vegetable("Carrot", 4.1, 16));
+            //((Vegetable)produce[2]).Name = "Carrot";
+            //((Vegetable)produce[2]).Weight = 4.1;
+            //((Vegetable)produce[2]).Quantity = 16;
+
+            Console.WriteLine("There are " + produce.Count + " items");
+
+            foreach (var item in produce)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+
+}
