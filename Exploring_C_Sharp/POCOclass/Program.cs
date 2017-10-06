f884ba7aba508d14991634ab73493666c07564d9
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-06 15:49:24 -0700

    Created a small POCO class

---
+++ Exploring_C_Sharp/POCOclass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace POCOclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var produce = new List<object>();
+
+            var f1 = new Fruit(); // No constructor, combiler will create no arg one
+            produce.Add(f1);
+
+            var f2 = new Fruit(); // No constructor, combiler will create no arg one
+            produce.Add(f2);
+
+            produce.Add(new Vegetable()); // addes a new with the () makes a new redefualt
+
+            Console.WriteLine("There are " + produce.Count + " items");
+
+            foreach (var item in produce)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+
+    class Fruit
+    {
+
+    }
+
+    class Vegetable
+    {
+
+    }
+}
