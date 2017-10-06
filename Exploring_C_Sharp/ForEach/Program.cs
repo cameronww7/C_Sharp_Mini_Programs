ba4bf85955518804b6fea05f7bad5e179a858a8f
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-06 13:26:59 -0700

    Finished Foreach and WhileLoop exploring

---
+++ Exploring_C_Sharp/ForEach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Each!");
+
+            string[] fruit = { "Apples", "Oranges", "Grapes", "Avocados" };
+
+            foreach (var item in fruit)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Exploring_C_Sharp/WhileLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(value: "While Loop!");
+
+            int counter = 1;
+            int max = 10;
+
+            while (counter <= max) {
+                Console.WriteLine("Loop number : " + counter);
+                counter++;
+            }
+
+            string welcome = "hello world";
+
+            counter = 0;
+
+            while( counter < welcome.Length) {
+                Console.WriteLine(welcome[counter]);
+                counter++;
+            }
+
+            counter = 0;
+
+            do {
+                Console.WriteLine("Loop number : " + counter);
+                counter++;
+            } while (counter <= max);
+        }
+    }
+}
