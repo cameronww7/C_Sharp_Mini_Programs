5048bbff13432f9d107ae248909e96c4026246a5
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-06 15:37:49 -0700

    Finished a small Dictionaraies program

---
+++ Exploring_C_Sharp/Dictionaries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionaries!");
+
+            var inventory = new Dictionary<string, double>();
+
+            inventory.Add("Figs", 56);
+            inventory.Add("Apples", 23);
+            inventory.Add("Oranges", 12);
+
+            var keys = inventory.Keys;
+
+            Console.WriteLine("Number of items: " + keys.Count);
+
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key + ": " + inventory[key]);
+            }
+
+            string[] keysArray = keys.ToArray();
+
+            Array.Sort(keysArray);
+
+            foreach (var key in keysArray)
+            {
+                Console.WriteLine(key + ": " + inventory[key]);
+            }
+
+            double value;
+            if (inventory.TryGetValue("Figs", out value))
+            {
+                Console.WriteLine("Figs inventory: " + value);
+            }
+            else
+            {
+                Console.WriteLine("Figs inventory not available");
+            }
+        }
+    }
+}
---
+++ Exploring_C_Sharp/Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic; // for List
+using System.Linq; // for .Contains
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lists!");
+
+            var fruitList = new List<string>();
+
+            fruitList.Add("Apple");
+            fruitList.Add("Orange");
+            fruitList.Add("Fig");
+
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+
+            fruitList.Sort();
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+
+            ReportOnFigs(fruitList);
+
+        }
+
+        static void ReportOnFigs(List<string> items)
+        {
+            string figReport =
+                items.Contains("Fig", StringComparer.OrdinalIgnoreCase) ?
+                "Yes there are figs." :
+                "No there are no figs";
+            Console.WriteLine(figReport);
+        }
+    }
+}
