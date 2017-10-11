8c2b9808c6df03affc5f31c25419a39b9dd37bdc
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-11 16:11:48 -0700

    More Try Catching with Do While taking in two numbers to divide them

---
+++ Exploring_C_Sharp/ExecptionsHandling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecptionsHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exceptions Handling!");
+
+            string errorMessage;
+
+            do {
+                errorMessage = "n/a";
+
+                int    numerator    = -1;
+                int    denominator  = -1;
+                int    result       = -1;
+
+                Console.WriteLine("Enter the numerator");
+                numerator = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("Enter the denominator");
+                denominator = Int32.Parse(Console.ReadLine());
+
+                try
+                {
+                    result = numerator / denominator;
+
+                    Console.WriteLine();
+                    Console.WriteLine("The result is: " + result);
+                }
+                catch (OverflowException ofEx)
+                {
+                    errorMessage = ofEx.Message;
+                    Console.WriteLine("\n" + errorMessage);
+                }
+                catch (DivideByZeroException dEx)
+                {
+                    errorMessage = dEx.Message;
+                    Console.WriteLine("\n" + errorMessage);
+                }
+                catch (Exception e)
+                {
+                    errorMessage = e.Message;
+                    Console.WriteLine("\n" + errorMessage);
+                }
+                Console.WriteLine("");
+
+            } while(errorMessage != "n/a");
+            
+        }
+    }
+}
