3b2da8f5231b8055f05175003c981eb22969e20d
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-11 16:22:07 -0700

    Created another divide by zero to step through the flow and watch what happens during exceptions

---
+++ Exploring_C_Sharp/ExceptionFlow/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ExceptionFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string errorMessage;
+                        
+            try
+            {
+                Divide();
+            }
+
+            catch (OverflowException ofEx)
+            {
+                errorMessage = ofEx.Message;
+                Console.WriteLine(errorMessage);
+            }
+            catch (DivideByZeroException dEx)
+            {
+                errorMessage = dEx.Message;
+                Console.WriteLine(errorMessage);
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                Console.WriteLine(errorMessage);
+            }
+
+            
+        }
+            
+        static void Divide()
+        {
+            int numerator;
+            int denominator;
+            int result;
+
+            Console.WriteLine("Enter the numerator");
+            numerator = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the denominator");
+            denominator = Int32.Parse(Console.ReadLine());
+
+            result = numerator / denominator;
+
+            Console.WriteLine();
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
