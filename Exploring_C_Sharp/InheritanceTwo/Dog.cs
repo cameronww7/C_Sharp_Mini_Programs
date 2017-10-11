7458e178427698fe1a9f3874b44bd208f07adb69
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-11 15:30:46 -0700

    Messing with more Inheritance

---
+++ Exploring_C_Sharp/InheritanceTwo/Dog.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace InheritanceTwo
+{
+    class Dog : Animal
+    {
+       
+
+    }
+}
---
+++ Exploring_C_Sharp/InheritanceTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InheritanceTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance Two!");
+
+            Animal dog = new Animal();
+            dog.Age = 2;
+            dog.Color = "Black";
+
+            Dog spot = new Dog();
+            spot.move(); // takes this class from Animal Class
+        }
+    }
+}
