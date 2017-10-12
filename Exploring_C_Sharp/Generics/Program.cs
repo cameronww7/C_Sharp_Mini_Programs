a3042ef9a6f5006f89b4b431ab6e82c8bd08d357
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-12 15:00:01 -0700

    Exploring a small example of Generics

---
+++ Exploring_C_Sharp/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics!");
+
+            Stack myStack = new Stack();
+
+            myStack.Push("A String");
+            myStack.Push(42);
+            myStack.Push(myStack);
+
+            myStack.Pop();
+
+            Stack<string> strString = new Stack<string>(); // self documentation
+
+            strString.Push("A string");
+            // strString.Push(42); // will error, expects a string
+
+        }
+    }
+}
