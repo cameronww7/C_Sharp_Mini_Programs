c9308b5835cf7a2b27dce8c400d68f1f930b1413
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-13 13:32:26 -0700

    Gonna explore some ALgorithms starting with linked List

---
+++ Exploring_Algorithms/Linked_List/Program.cs
@@ -0,0 +1,19 @@
+﻿/***********************************************************************
+ *              Cameron Walters
+ *                      C# Exploring C#
+ ***********************************************************************/
+
+using System;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked_List!");
+
+
+        }
+    }
+}
