9bc7b92b3278ba5ecebf49b841e1fef03062528e
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-13 13:49:31 -0700

    Finished Linked_list then exploring built in linked list

--- Exploring_Algorithms/Linked_List/Program.cs
+++ Exploring_Algorithms/Linked_List/Program.cs
@@ -5,15 +5,86 @@
 
 using System;
 
-namespace Linked_List
+namespace Singly_Linked_List
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Linked_List!");
+            Console.WriteLine("Singly Linked_List!");
+            SinglyLinkedList myList = new SinglyLinkedList();
 
+            myList.insertFirst(100);
+            myList.insertFirst(99);
+            myList.insertFirst(98);
+            myList.insertFirst(97);
+            myList.insertFirst(96);
+            myList.insertLast(101);
+            myList.displayList();
 
+            myList.deleteFirst();
+            myList.displayList();
+
+            myList.displayList();
+            myList.displayList();
+
+        }
+    }
+
+    public class SinglyLinkedList
+    {
+        private Node first;
+        public bool isEmpty()
+        {
+            return (first == null);
+        }
+
+        public void insertFirst(int data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+            newNode.next = first;
+            first = newNode;
+        }
+
+        public Node deleteFirst()
+        {
+            Node temp = first;
+            first = first.next;
+            return temp;
+        }
+
+        public void displayList () {
+            Console.WriteLine("List (First -- Last) ");
+            Node current = first;
+            while (current != null) {
+                current.displayNode();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+
+        public void insertLast(int data) {
+            Node current = first;
+            while ( current.next != null) {
+                current = current.next;
+            }
+
+            Node newNode = new Node();
+            newNode.data = data;
+            current.next = newNode;
+        }
+
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public void displayNode()
+        {
+            Console.WriteLine(" < " + data + " > ");
         }
     }
 }
---
+++ Exploring_Algorithms/Linked_List_DotNet_Collections/Program.cs
@@ -0,0 +1,108 @@
+﻿/***********************************************************************
+ *              Cameron Walters
+ *                      C# Exploring C#
+ ***********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_DotNet_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked_List_DotNet_Collections!");
+
+            DemoLinkedListFeatures();
+        }
+      
+        private static void DemoLinkedListFeatures()
+        {
+            
+            string[] words = { "the", "actor", "jumped", "over", "the", "director" };
+            LinkedList<string> sentence = new LinkedList<string>(words);
+            Display(sentence, "The linked list values:");
+
+                        
+            sentence.AddFirst("today");
+            Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+            
+            LinkedListNode<string> mark1 = sentence.First;
+            sentence.RemoveFirst();
+            sentence.AddLast(mark1);
+            Display(sentence, "Test 2: Move first node to be last node:");
+            
+            sentence.RemoveLast();
+            sentence.AddLast("yesterday");
+            Display(sentence, "Test 3: Change the last node to 'yesterday':");
+            
+            mark1 = sentence.Last;
+            sentence.RemoveLast();
+            sentence.AddFirst(mark1);
+            Display(sentence, "Test 4: Move last node to be first node:");
+
+            
+            sentence.RemoveFirst();
+            LinkedListNode<string> current = sentence.FindLast("the");
+            IndicateNode(current, "Test 5: Indicate last occurence of 'the':");
+            
+            sentence.AddAfter(current, "old");
+            sentence.AddAfter(current, "lazy");
+            IndicateNode(current, "Test 6: Add 'lazy' and 'old' after 'the':");
+            
+            current = sentence.Find("actor");
+            IndicateNode(current, "Test 7: Indicate the 'actor' node:");
+            
+            sentence.AddBefore(current, "quick");
+            sentence.AddBefore(current, "skinny");
+            IndicateNode(current, "Test 8: Add 'quick' and 'skinny' before 'actor':");
+
+        
+            Console.ReadLine();
+        }
+
+        private static void Display(LinkedList<string> words, string test)
+        {
+            Console.WriteLine(test);
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void IndicateNode(LinkedListNode<string> node, string test)
+        {
+            Console.WriteLine(test);
+            if (node.List == null)
+            {
+                Console.WriteLine("Node '{0}' is not in the list.\n",
+                    node.Value);
+                return;
+            }
+
+            StringBuilder result = new StringBuilder("(" + node.Value + ")");
+            LinkedListNode<string> nodeP = node.Previous;
+
+            while (nodeP != null)
+            {
+                result.Insert(0, nodeP.Value + " ");
+                nodeP = nodeP.Previous;
+            }
+
+            node = node.Next;
+            while (node != null)
+            {
+                result.Append(" " + node.Value);
+                node = node.Next;
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
+    }
+}
