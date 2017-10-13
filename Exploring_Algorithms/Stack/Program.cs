6f18f7fe743bfbc57b3c673fe3ceb909ac62d25b
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-13 14:04:51 -0700

    Created a Stack and then used Collections to see built in stack

---
+++ Exploring_Algorithms/Stack/Program.cs
@@ -0,0 +1,83 @@
+﻿﻿/***********************************************************************
+ *              Cameron Walters
+ *                      C# Exploring C#
+ ***********************************************************************/
+
+using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack!");
+
+            Stack theStack = new Stack(4);
+            theStack.push("Star Wars");
+            theStack.push("Jaws");
+            theStack.push("Jaws 2");
+            theStack.push("Titanic");
+            theStack.push("Rocky");
+
+            Console.WriteLine("================== \nAbout to peek:");
+            Console.WriteLine("'" + theStack.peek() + "'" + " is at the top of the stack.\n");
+
+            Console.WriteLine("================== \nThe Stack contains:\n");
+            while (!theStack.isEmpty())
+            {
+                string movie = theStack.pop();
+                Console.WriteLine(movie);
+            }
+        }
+        public class Stack 
+        {
+            private int maxSize;
+            private string[] stackArray;
+            private int top;
+
+            public Stack(int size)
+            {
+                maxSize = size;
+                stackArray = new string[maxSize];
+                top = -1;
+            }
+
+            public void push(string m) {
+                if (isFull())
+                {
+                    Console.WriteLine("This stack is full");
+                } else {
+                    top++;
+                    stackArray[top] = m;
+                }
+            }
+
+            public string pop() {
+                if (isEmpty())
+                {
+                    Console.WriteLine("The stack is empty.");
+                    return "--";
+                } else {
+                    int old_top = top;
+                    top--;
+                    return stackArray[old_top];
+                }
+            }
+
+            public string peek() {
+                return stackArray[top];
+            }
+
+            public bool isEmpty()
+            {
+                return (top == -1);
+            }
+
+            private bool isFull()
+            {
+                return (maxSize - 1 == top);
+            }
+        }
+    }
+}
---
+++ Exploring_Algorithms/Stack_DotNet_Collections/Program.cs
@@ -0,0 +1,84 @@
+﻿﻿/***********************************************************************
+ *              Cameron Walters
+ *                      C# Exploring C#
+ ***********************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Stack_DotNet_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DemoStack();
+        }
+        
+        private static void DemoStack()
+        {
+            Stack<string> names = new Stack<string>();
+            names.Push("Andrew");
+            names.Push("Bobby");
+            names.Push("Candy");
+            names.Push("Dana");
+            names.Push("Edith");
+            
+            Console.WriteLine("Default content of stack");
+            
+            foreach (string name in names)
+            {                
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nPopping '{0}'", names.Pop());
+            Console.WriteLine("Peek at next item to display what will" +
+                              " be destacked: {0}",names.Peek());
+            Console.WriteLine("Now, after peek, popping '{0}'", names.Pop());
+
+            
+            Stack<string> stack2 = new Stack<string>(names.ToArray());
+
+            Console.WriteLine("\nContents of the first copy:");
+            foreach (string name in stack2)
+            {
+                Console.WriteLine(name);
+            }
+            
+            string[] array2 = new string[names.Count * 2];
+            names.CopyTo(array2, names.Count);
+            
+            Stack<string> stack3 = new Stack<string>(array2);
+
+            Console.WriteLine("\nContents of the second copy, with " +
+                              "duplicates and nulls:");
+            foreach (string name in stack3)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nstack2.Contains(\"Dana\") = {0}",
+                stack2.Contains("Dana"));
+
+            Console.WriteLine("\nstack2.Clear()");
+            stack2.Clear();
+            Console.WriteLine("\nstack2.Count = {0}", stack2.Count);
+        }
+
+
+        private static void Count(int a)
+        {
+            Console.WriteLine("a = " + a + " on the stack");
+            if (a < 3)
+            {
+                Count(a + 1);
+            }
+            Console.WriteLine("a = " + a + " off the stack");
+
+        }
+
+    }
+}
