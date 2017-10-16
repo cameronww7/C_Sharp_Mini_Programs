1ef781c38f6939affa43d91756ed4e4efb72db6a
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-16 09:23:20 -0700

    Created a Queue

---
+++ Exploring_Algorithms/Queue/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue!");
+
+            Queue myQueue = new Queue(5);
+            myQueue.insert(100);
+            myQueue.insert(10);
+            myQueue.insert(20);
+            myQueue.insert(30);
+            myQueue.view();
+
+            Console.WriteLine($"Front of queue is {myQueue.peekFront()} ");
+
+            Console.WriteLine("About to remove item from queue");
+            myQueue.remove();
+            Console.WriteLine($"Front of queue is {myQueue.peekFront()} ");
+        }
+    }
+
+    public class Queue
+    {
+        private int maxSize; // of queue
+        private long[] myQueue;
+        private int front; // of queue
+        private int rear; // of queue
+        private int items; // in the queue
+
+        public Queue(int size)
+        {
+            maxSize = size;
+            myQueue = new long[size];
+            front = 0;
+            rear = -1;
+            items = 0;
+        }
+
+        public void insert(long j)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Queue is full");
+            }
+            else
+            {
+                if (rear == maxSize - 1)
+                {
+                    rear = -1;
+                }
+                rear++;
+                myQueue[rear] = j;
+                items++;
+            }
+        }
+
+        public long remove()
+        {
+            long temp = myQueue[front];
+            front++;
+            if (front == maxSize)
+            {
+                front = 0;
+            }
+            return temp;
+        }
+
+        public long peekFront()
+        {
+            return myQueue[front];
+        }
+
+        public bool isEmpty()
+        {
+            return (items == 0);
+        }
+
+        private bool isFull()
+        {
+            return (items == maxSize);
+        }
+
+        public void view() {
+            Console.Write("[");
+            for (int i = 0; i < myQueue.Length; i++)
+            {
+                Write(myQueue[i] + " ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
